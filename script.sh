ef0919dd352955970e9a21a49eb1035b80fda29b
Tembi <ndahtembi@gmail.com>
2025-03-07 21:35:06 +0100

    3rd commit

--- script.sh
+++ script.sh
@@ -1,3 +1,6 @@
+#!/bin/bash
+
+# install jenkins
 sudo apt update
 sudo apt install openjdk-11-jdk -y
 wget -q -O - https://pkg.jenkins.io/debian/jenkins.io.key | sudo tee \
@@ -6,3 +9,21 @@ wget -q -O - https://pkg.jenkins.io/debian/jenkins.io.key | sudo tee \
   /etc/apt/sources.list.d/jenkins.list > /dev/null
 sudo apt update
 sudo apt install jenkins -y
+sudo su && echo "jenkins ALL=(ALL) NOPASSWD: ALL" >> /etc/sudoers
+sudo systemctl enable jenkins
+sudo systemctl start jenkins
+sudo systemctl status jenkins
+
+# install git
+sudo apt install git -y
+
+# install terraform
+sudo apt install -y apt-utils
+sudo apt-config-manager --add-repo https://rpm.releases.hashicorp.com/AmazonLinux/hashicorp.repo
+sudo apt -y install terraform
+
+#install ansible
+$ sudo apt update
+$ sudo apt install software-properties-common
+$ sudo add-apt-repository --yes --update ppa:ansible/ansible
+$ sudo apt install ansible
