4549405a5b4c5b433ff93d64979e35c159472b4a
Tembi <ndahtembi@gmail.com>
2025-03-07 15:03:57 +0100

    ingress added

--- main.tf
+++ main.tf
@@ -20,8 +20,20 @@ resource "aws_security_group" "web_sg" {
     protocol    = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
   }
-}
 
+   ingress {
+    from_port   = 8080
+    to_port     = 8080
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+}
+}
 # Load Balancer
 resource "aws_lb" "web_alb" {
   name               = "web-app-lb"
