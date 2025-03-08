29f7714d32ec10fae8252795e29e80a6335571d0
Tembi <ndahtembi@gmail.com>
2025-03-08 21:47:27 +0100

    file updated

--- main.tf
+++ main.tf
@@ -7,7 +7,7 @@ resource "aws_vpc" "web_vpc" {
 resource "aws_subnet" "public1" {
   vpc_id            = aws_vpc.web_vpc.id
   cidr_block        = "10.0.1.0/24"
-  availability_zone = "en-north-1a"
+  availability_zone = "en-north-1"
 }
 
 # Security Group
